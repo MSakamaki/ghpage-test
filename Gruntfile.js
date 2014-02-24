0ad622bb2875edace9842494407e3608ab9835d0
SAKAMAKI Mizuho <msakamaki@fxos-code-reading.z1d2.static.cnode.jp>
2014-02-24 13:01:40 +0900

    sec commit

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+// Generated on 2014-02-24 using generator-angular 0.7.1
+'use strict';
+
+module.exports = function (grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  require('time-grunt')(grunt);
+
+  grunt.initConfig({
+    'gh-pages': {
+      options: {
+        base: 'public'
+      },
+      src: ['**']
+    }
+  });
+};
+
